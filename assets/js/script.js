13e3ca672a091caa2ea475c331d9a9ad35116c8a
Gabriel Cavalcante <gabriel.f.cavalcante@hotmail.com>
2021-06-07 18:05:28 -0300

    Criando tabuleiro via DOM e adicionando classes e IDs para identificação de cada célula.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,30 @@
 let start__button = document.getElementById('start')
 
 // BOTÃO DE INICIAR O JOGO
-start__button.addEventListener('click', function() {
+start__button.addEventListener('click', function () {
     let modal = document.getElementsByClassName('start__screen')
+    gameStart()
     modal[0].style.display = "none"
 })
+
+
+function gameStart() {
+    let mainscreen = document.getElementById('table')
+
+    for (let i = 0; i < 7; i++) {
+        let coluna = document.createElement('div')
+        coluna.classList.add('table__column')
+        coluna.id = i
+        mainscreen.appendChild(coluna)
+    }
+
+    for (let i = 0; i < 7; i++) {
+        let colunaAtual = document.getElementById(`${i}`)
+        for (let j = 0; j < 5; j++) {
+            let celula = document.createElement('div')
+            celula.classList.add('table__cell')
+            celula.id = i + "." + j
+            colunaAtual.appendChild(celula)
+        }
+    }
+}
