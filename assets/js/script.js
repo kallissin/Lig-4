6d5af8e741dc92680397258df413229653bb9221
Gabriel Cavalcante <gabriel.f.cavalcante@hotmail.com>
2021-06-07 17:28:00 -0300

    Adicionando tela inicial, botão de iniciar e tabuleiro de jogo (ainda fixo no HTML).

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let start__button = document.getElementById('start')
+
+// BOTÃO DE INICIAR O JOGO
+start__button.addEventListener('click', function() {
+    let modal = document.getElementsByClassName('start__screen')
+    modal[0].style.display = "none"
+})
