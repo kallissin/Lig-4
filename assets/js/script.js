cacd0485195b7a265b51956d140a875f9ab643dd
Leandro <legato_soares@hotmail.com>
2021-06-08 09:03:54 -0300

    adicionando funcao de turnos e de posicionamento dos discos

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,11 +34,46 @@ function gameStart() {
 
     for (let i = 0; i < 7; i++) {
         let colunaAtual = document.getElementById(`${i}`)
-        for (let j = 0; j < 5; j++) {
+        for (let j = 0; j < 6; j++) {
             let celula = document.createElement('div')
             celula.classList.add('table__cell')
             celula.id = i + "." + j
             colunaAtual.appendChild(celula)
         }
+    }
+
+    // Pegar as colunas e dar eventos respectivos 
+    const colunas = document.querySelectorAll("div.table__column")
+    let escolha = true 
+    for (let i = 0; i < colunas.length; i++) {
+        colunas[i].addEventListener("click" , function(){
+            const disco = document.createElement("div")
+           
+
+            for (let j = 0;j < 6;j++) {
+                
+                let cell = colunas[i].children[j]
+
+
+                if (cell.childElementCount == 0){
+                    console.log(escolha)
+                if ( escolha === true ){
+                    disco.classList = "red"
+                    cell.appendChild(disco)
+                }else{
+                    disco.classList = "black"
+                    cell.appendChild(disco)
+                }
+            }
+            if (escolha == true){
+                escolha = false
+            }else{
+                escolha = true 
+            }
+        }
+ 
+            
+        })
+        
     }
 }
