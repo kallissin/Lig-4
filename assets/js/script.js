75ca8566e15fa61484f7c1e3897cd049549cc119
Gabriel Cavalcante <gabriel.f.cavalcante@hotmail.com>
2021-06-08 15:24:26 -0300

    Funções de vitória vertical e horizontal criadas. Falta diagonal.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,11 @@ let start__button = document.getElementById('start')
 // BOTÃO DE INICIAR O JOGO
 start__button.addEventListener('click', function () {
     let modal = document.getElementsByClassName('start__screen')
-    gameStart()
+    getName()
     modal[0].style.display = "none"
 })
 
+// FUNÇÃO DE COLETAR NOME NA TELA INICIAL
 function getName() {
     let input = document.getElementById('name')
     let name = input.value
@@ -17,11 +18,12 @@ function getName() {
     let nameText = document.createElement('h1')
     nameText.innerText = name
     player.appendChild(nameText)
+    return name
 }
 
+// FUNÇÃO DE CRIAR TABULEIRO DE JOGO
 function gameStart() {
     let mainscreen = document.getElementById('table')
-    getName()
 
     for (let i = 0; i < 7; i++) {
         let coluna = document.createElement('div')
@@ -39,33 +41,115 @@ function gameStart() {
             colunaAtual.appendChild(celula)
         }
     }
+}
 
-    // Pegar as colunas e dar eventos respectivos 
-    const colunas = document.querySelectorAll("div.table__column")
-    let escolha = true
-    for (let i = 0; i < colunas.length; i++) {
-        colunas[i].addEventListener("click", function () {
-            const disco = document.createElement("div")
+gameStart()
 
-            for (let j = 0; j < 6; j++) {
-                let cell = colunas[i].children[j]
-                if (cell.childElementCount == 0) {
-                    console.log(escolha)
-                if ( escolha === true ){
+// Pegar as colunas e dar eventos respectivos
+let lastplay = 0
+let colunas = document.querySelectorAll("div.table__column")
+let escolha = true
+for (let i = 0; i < colunas.length; i++) {
+    colunas[i].addEventListener("click", function () {
+        const disco = document.createElement("div")
+
+        for (let j = 0; j < 6; j++) {
+            let cell = colunas[i].children[j]
+            if (cell.childElementCount === 0) {
+                if (escolha === true) {
                     disco.classList = "red"
                     cell.appendChild(disco)
+                    lastplay = cell
+                    if (winCheckVertical(lastplay) === true) { console.log(getName() + ' venceu') }
+                    if (winCheckRight(lastplay) === true) { console.log(getName() + ' venceu') }
+                    if (winCheckLeft(lastplay) === true) { console.log(getName() + ' venceu') }
                     escolha = false
                     break
-                }else{
+                } else {
                     disco.classList = "black"
                     cell.appendChild(disco)
+                    lastplay = cell
+                    if (winCheckVertical(lastplay) === true) { console.log('Preto venceu') }
+                    if (winCheckRight(lastplay) === true) { console.log('Preto venceu') }
+                    if (winCheckLeft(lastplay) === true) { console.log('Preto venceu') }
                     escolha = true
                     break
                 }
-            } 
+            }
         }
- 
-            
-        })
+    })
+}
+
+function winCheckVertical(cell) {
+    let id = cell.id
+    let column = Number(id.charAt(0))
+    let place = Number(id.charAt(2))
+
+    let colour = cell.lastChild.classList[0]
+    let count = 0
+
+    for (let i = 1; i < 4; i++) { // CHECK VERTICAL
+        let next = document.getElementById(`${column}.${place - i}`)
+
+        if (next === null) {
+            return false
+        } else if (next.lastChild.classList[0] === colour) {
+            count += 1
+        }
     }
+
+    if (count === 3) {
+        return true
+    }
+    return false
+}
+
+function winCheckRight(cell) {
+    let id = cell.id
+    let column = Number(id.charAt(0))
+    let place = Number(id.charAt(2))
+
+    let colour = cell.lastChild.classList[0]
+    let countRight = 0
+    let countLeft = 0
+
+    for (let i = 1; i < 4; i++) { // CHECK VERTICAL
+        let next = document.getElementById(`${column + i}.${place}`)
+
+        if (next === null || next.lastChild === null) {
+            return false
+        } else if (next.lastChild.classList[0] === colour) {
+            countRight += 1
+        }
+    }
+
+    if (countRight === 3) {
+        return true
+    }
+    return false
+}
+
+function winCheckLeft(cell) {
+    let id = cell.id
+    let column = Number(id.charAt(0))
+    let place = Number(id.charAt(2))
+
+    let colour = cell.lastChild.classList[0]
+    let countRight = 0
+    let countLeft = 0
+
+    for (let i = 1; i < 4; i++) { // CHECK VERTICAL
+        let next = document.getElementById(`${column - i}.${place}`)
+
+        if (next === null || next.lastChild === null) {
+            return false
+        } else if (next.lastChild.classList[0] === colour) {
+            countRight += 1
+        }
+    }
+
+    if (countRight === 3) {
+        return true
+    }
+    return false
 }
