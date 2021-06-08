c3b3e9ab422e72201154f365d01e7b6cf7ae05d7
Gabriel Cavalcante <gabriel.f.cavalcante@hotmail.com>
2021-06-07 22:21:05 -0300

    Adicionando input para pegar nome do jogador. Nomes maiores que 8 caracteres são cortados para 8 caracteres seguidos de '...'

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,23 @@ start__button.addEventListener('click', function () {
     modal[0].style.display = "none"
 })
 
+function getName() {
+    let input = document.getElementById('name')
+    let name = input.value
+
+    if (name.length > 8) {
+        name = name.substring(0, 8) + "..."
+    }
+
+    let player = document.getElementById('jogador')
+    let nameText = document.createElement('h1')
+    nameText.innerText = name
+    player.appendChild(nameText)
+}
 
 function gameStart() {
     let mainscreen = document.getElementById('table')
+    getName()
 
     for (let i = 0; i < 7; i++) {
         let coluna = document.createElement('div')
